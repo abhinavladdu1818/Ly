7ac6ebfdfe599fbcb579142ad888cf1318010dd2
abhinavladdu1818 <laddumudhiraj1818@gmail.com>
2025-01-31 10:39:12 +0530

    Add files via upload

---
+++ Ly.java
@@ -0,0 +1,15 @@
+
+    import java.util.Scanner;
+public class Ly {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("enter the year");
+        int year=scanner.nextInt();
+        if(year%400==0){
+            System.out.println("leap year");
+        }else{
+            System.out.println("not a leap year");
+        }
+        scanner.close();}
+}
+
